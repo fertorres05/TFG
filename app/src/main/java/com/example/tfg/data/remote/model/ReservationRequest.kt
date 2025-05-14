eb8a354b70094a42dfcfdaecfddf120352294906
fernando <fernandotorresserra05@gmail.com>
2025-05-14 18:46:55 +0200

    Pantalla de home funcional con su view model, flighInfoScreen

--- app/src/main/java/com/example/tfg/data/remote/model/ReservationRequest.kt
+++ app/src/main/java/com/example/tfg/data/remote/model/ReservationRequest.kt
@@ -1,6 +1,6 @@
 package com.example.tfg.data.remote.model
 
-data class FlightRequest(
+data class ReservationRequest(
     val codeflight: String,
     val uuid_user: String,
     val persons: Int? = null,
